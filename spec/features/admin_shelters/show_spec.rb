d58fd9e0787bae8038947a14346f9e49c8a5a949
Jenny Branham <branhamjenny@gmail.com>
2021-02-15 17:24:58 -0600

    Add raw sql queries

---
+++ spec/features/admin_shelters/show_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin Shelter show page' do
+  before :each do
+    @shelter1 = Shelter.create!(name: "Shady Shelter", address: "123 Shady Ave", city: "Denver", state: "CO", zip: 80011)
+  end
+
+  it "displays shelter with that id and all its attributes" do
+    visit "/admin/shelters/#{@shelter1.id}"
+
+    expect(page).to have_content(@shelter1.name)
+    expect(page).to have_content(@shelter1.address)
+    expect(page).to have_content(@shelter1.city)
+    expect(page).to have_content(@shelter1.state)
+    expect(page).to have_content(@shelter1.zip)
+  end
+end
