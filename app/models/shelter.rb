5a855f0f2b792237a4e67a3a99a51fe98c3d0511
Jenny Branham <branhamjenny@gmail.com>
2021-02-16 14:34:22 -0600

    Add avg age for adoptable pets at a shelter

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,7 +1,7 @@
 class Shelter < ApplicationRecord
   has_many :pets
   validates_uniqueness_of :name
-  
+
   def self.by_reverse_alphabetical
     find_by_sql "select * from shelters order by name desc;"
   end
@@ -15,5 +15,13 @@ class Shelter < ApplicationRecord
     .where('applications.status' => 1)
     .select('distinct shelters.*')
     .order('shelters.name')
+  end
+
+  def average_age_for_adoptable_pets
+    # .joins(pets: [{application_pets: :application}])
+    # .where('pets.adoptable' => true)
+    # .select("pets.*")
+    # .average("pets.approximate_age")
+    pets.where(adoptable: true).average(:approximate_age)
   end
 end
