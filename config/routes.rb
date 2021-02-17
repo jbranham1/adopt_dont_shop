f018dce16416706b81aaa905b1016a707fe695e6
Jenny Branham <branhamjenny@gmail.com>
2021-02-17 13:41:47 -0600

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,38 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: "welcome#index"
 
-  get "/shelters", to: "shelters#index"
-  get "/shelters/new", to: "shelters#new"
-  get "/shelters/:id", to: "shelters#show"
-  delete "/shelters/:id", to: "shelters#destroy"
-  post "/shelters", to: "shelters#create"
-  get "/shelters/:id/edit", to: "shelters#edit"
-  patch "/shelters/:id", to: "shelters#update"
-
-
-  get "/pets", to: "pets#index"
-  get "/pets/:id", to: "pets#show"
-  get "/pets/:id/edit", to: "pets#edit"
-  patch "/pets/:id", to: "pets#update"
-  delete "/pets/:id", to: "pets#destroy"
+  resources :shelters, :pets, :applications, :application_pets
+  resources :admin_applications, path: '/admin/applications'
+  resources :admin_applications, path: '/admin/applications'
+  resources :admin_shelters, path: '/admin/shelters'
 
   get "/shelters/:shelter_id/pets", to: "shelter_pets#index"
   get "/shelters/:shelter_id/pets/new", to: "shelter_pets#new"
   post "/shelters/:shelter_id/pets", to: "shelter_pets#create"
   get "/shelters/:id/pets/:id", to: "pets#show"
-
-  get "/applications/new", to: "applications#new"
-  get "/applications/:id", to: "applications#show"
-  get "/applications/:id/edit", to: "applications#edit"
-  patch "/applications/:id", to: "applications#update"
-  post "/applications", to: "applications#create"
-  delete "/applications/:id", to: "applications#destroy"
-
-  post "/application_pets", to: "application_pets#create"
-  get "/admin/applications/", to: "admin_applications#index"
-  get "/admin/applications/:id", to: "admin_applications#show"
-  patch "/admin/applications/:id", to: "admin_applications#update"
-
-  get "/admin/shelters", to: "admin_shelters#index"
-  get "/admin/shelters/:id", to: "admin_shelters#show"
 end
