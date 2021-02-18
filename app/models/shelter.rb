6ab474547d4cbc539e54d0e55fd6744c470fb53a
Jenny Branham <branhamjenny@gmail.com>
2021-02-18 09:01:38 -0600

    change active record

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -13,9 +13,9 @@ class Shelter < ApplicationRecord
 
   def self.shelters_with_pending_applications
     joins(pets: [{application_pets: :application}])
-    .where('applications.status' => 1)
-    .select('distinct shelters.*')
-    .order('shelters.name')
+    .where(applications: {status: 1})
+    .order(:name)
+    .distinct
   end
 
   def average_age_for_adoptable_pets
